d4beb1026de597bf91833b75c498d09c67aee485
MedvedevMax <maxmedvedev@me.com>
2014-03-03 01:33:07 +0400

    Welcome View Controller prototype

--- InstaReview/IRWelcomeViewController.h
+++ InstaReview/IRWelcomeViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface IRViewController : UIViewController
+@interface IRWelcomeViewController : UIViewController
 
 @end
---
+++ InstaReview/IRWelcomeViewController.m
@@ -0,0 +1,28 @@
+//
+//  IRViewController.m
+//  InstaReview
+//
+//  Created by Max Medvedev on 3/3/14.
+//  Copyright (c) 2014 Max Medvedev. All rights reserved.
+//
+
+#import "IRWelcomeViewController.h"
+
+@interface IRWelcomeViewController ()
+
+@end
+
+@implementation IRWelcomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)snapTapped
+{
+    
+}
+
+
+@end
